1523bab96ca732392c307899590d95df9eaa6254
mohsen <mohsen1076@gmail.com>
2022-10-28 10:05:57 +0330

    Add a JS function to sort arrays into tools.js

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,11 +1,16 @@
 import {
     arraySum,
     arrayMin,
-    arrayMax
+    arrayMax,
+    sortArray
 } from './tools.js';
+
 
 const numbers = [1, -2, 3, 4, 12, 0, -5, 8, 12];
 
 console.log(`sum of [${numbers}] --> ${arraySum(numbers)}`);
 console.log(`min of [${numbers}] --> ${arrayMin(numbers)}`);
 console.log(`max of [${numbers}] --> ${arrayMax(numbers)}`);
+
+// sorting numbers using sortArray
+console.log(`sorted of [${numbers}] --> [${sortArray(numbers)}]`);
--- Javascript/tools.js
+++ Javascript/tools.js
@@ -8,8 +8,34 @@ const arrayMin = (arr) => arr.reduce((t, v) => t > v ? v : t);
 
 const arrayMax = (arr) => arr.reduce((t, v) => t > v ? t : v);
 
+
+const sortArray = function (arr) {
+    const sorted = [];
+    let len = arr.length;
+
+    for (let i = 0; i < len; i++) {
+        let min_index = 0;
+        let min_value;
+        let temp;
+        min_value = arr.slice(i).reduce((t, v, index) => {
+            if (t > v) {
+                t = v;
+                min_index = index;
+            }
+            return t
+        })
+
+        temp = arr[i];
+        arr[i] = min_value;
+        arr[min_index + i] = temp;
+    }
+
+    return arr;
+}
+
 export {
     arraySum,
     arrayMin,
-    arrayMax
+    arrayMax,
+    sortArray,
 }
